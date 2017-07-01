7d4c3ebb64c7c5623d66e36d0c88cf9da6b3976b
Nate Smith <nwjsmith@gmail.com>
2017-06-30 20:04:51 -0400

    Fix up for Maven Central

--- project.clj
+++ project.clj
@@ -1,8 +1,14 @@
-(defproject com.theinternate.reformatory "0.1.0-SNAPSHOT"
+(defproject com.theinternate/reformatory "0.1.0-SNAPSHOT"
   :description "Coming soon."
   :url "https://github.com/nwjsmith/reformatory"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:name "git"
+        :tag "HEAD"
+        :url "https://github.com/nwjsmith/reformatory"}
+  :pom-addition [:developers [:developer {:id "nwjsmith"}
+                              [:name "Nate Smith"]
+                              [:url "http://theinternate.com"]]]
   :pedantic? :abort
   :global-vars {*warn-on-reflection* true}
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
